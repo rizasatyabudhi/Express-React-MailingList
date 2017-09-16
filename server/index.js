0f1e06dc6c5bd24db3281b3ee4004017cf22ac45
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-09-17 01:48:21 +0700

    initialize mongoose

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes.js");
+const keys = require("./config/keys.js");
 require("./services/passport.js");
 
 const app = express();
+
+// required to connect mongoose with our mongoDB
+mongoose.connect(keys.mongoURI);
 
 authRoutes(app);
 
